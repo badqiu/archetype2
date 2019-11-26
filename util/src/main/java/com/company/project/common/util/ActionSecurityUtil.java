a33a98033dda85e67230bd23ce36d5a1e961fa37
badqiu <badqiu@gmail.com>
2019-11-26 17:21:45 +0800

    1

---
+++ util/src/main/java/com/company/project/common/util/ActionSecurityUtil.java
@@ -0,0 +1,35 @@
+package com.company.project.common.util;
+
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ActionSecurityUtil {
+
+	public static void checkAction(HttpServletRequest request,Class<?> actionType,String permission) {
+		checkAction(request,actionType.getSimpleName(),permission);
+	}
+
+	public static void checkAction(HttpServletRequest request, String actionType,String permission) {
+		long userId = getUserId(request);
+		Set userPermissionSet = getUserPermissionSet(userId,request);
+		
+		if(userPermissionSet.contains(actionType+":"+permission)) {
+			return;
+		}else {
+			throw new SecurityException("not permission,actionType:"+actionType+" permission:"+permission+" userId:"+userId);
+		}
+	}
+	
+	/** 请自行实现
+	 *  得到用户拥有的权限集合 
+	 **/
+	private static Set getUserPermissionSet(long userId,HttpServletRequest request) {
+		return (Set)request.getSession().getAttribute("LOGIN_USER_PERMISSION");
+	}
+
+	private static long getUserId(HttpServletRequest request) {
+		return (Long)request.getSession().getAttribute("LOGIN_USER_ID");
+	}
+	
+}
