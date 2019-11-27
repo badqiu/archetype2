379231b4e6b8c3c91f9d312b22db005d740c0da1
badqiu <badqiu@gmail.com>
2019-11-27 16:30:20 +0800

    1

--- util/src/main/java/com/company/project/common/util/ActionSecurityUtil.java
+++ util/src/main/java/com/company/project/common/util/ActionSecurityUtil.java
@@ -2,10 +2,22 @@ package com.company.project.common.util;
 
 import java.util.Set;
 
+
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 用户管理权限检查，检查某类动作的读写权限
+ * 
+ * r: 读权限
+ * w: 写权限(增删修改)
+ * 
+ * @author badqiu
+ *
+ */
 public class ActionSecurityUtil {
-
+	private static String LOGIN_USER_PERMISSION = "LOGIN_USER_PERMISSION";
+	private static String LOGIN_USER_ID = "LOGIN_USER_ID";
+	
 	/**
 	 * 检查读写权限,没有权限抛出 SecurityException
 	 * 
@@ -31,11 +43,21 @@ public class ActionSecurityUtil {
 	 *  得到用户拥有的权限集合 
 	 **/
 	private static Set getUserPermissionSet(long userId,HttpServletRequest request) {
-		return (Set)request.getSession().getAttribute("LOGIN_USER_PERMISSION");
+		return (Set)request.getSession().getAttribute(LOGIN_USER_PERMISSION);
 	}
 
 	private static long getUserId(HttpServletRequest request) {
-		return (Long)request.getSession().getAttribute("LOGIN_USER_ID");
+		return (Long)request.getSession().getAttribute(LOGIN_USER_ID);
+	}
+	
+	/** 
+	 *  用户登录成功,设置登录成功信息
+	 *  @param userId 登录用户ID
+	 *  @param userPermissionSet 登录用户的权限集合,格式示例为:  sometype:w , sometype:r
+	 **/
+	public static void setLoginUserInfo(HttpServletRequest request,long userId,Set userPermissionSet) {
+		request.getSession().setAttribute(LOGIN_USER_ID,userId);
+		request.getSession().setAttribute(LOGIN_USER_PERMISSION,userPermissionSet);
 	}
 	
 }
