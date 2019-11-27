b718e29e986991c7f04d0448a8944ab3a1288ef9
badqiu <badqiu@gmail.com>
2019-11-27 17:45:31 +0800

    1

--- admin-server/src/main/java/com/company/project/admin/controller/BaseController.java
+++ admin-server/src/main/java/com/company/project/admin/controller/BaseController.java
@@ -7,13 +7,42 @@ import com.company.project.common.util.ActionSecurityUtil;
 public class BaseController {
 
 	/**
-	 * 检查读写权限,没有权限抛出 SecurityException
+	 * 检查动作读写权限(表读写权限),没有权限抛出 SecurityException
 	 * 
 	 * @param actionType 动作分类
 	 * @param permission 增删修改=w(写权限), 查=r(读权限)
 	 */
 	protected void checkActionPermission(HttpServletRequest request,Class<?> actionType, String permission) {
 		ActionSecurityUtil.checkActionPermission(request, actionType, permission);
+	}
+	
+	/**
+	 * 检查实体读写权限(数据行读写权限),没有权限抛出 SecurityException
+	 * 
+	 * @param entity 实体对象(数据行)
+	 * @param permission 增删修改=w(写权限), 查=r(读权限)
+	 */
+	protected void checkEntityPermission(HttpServletRequest request,Object entity, String permission) {
+		//检查读权限
+		if("r".equals(permission)){
+			return;
+		}
+		
+		//检查写权限
+		if("w".equals(permission)){
+			/*
+			示例代码,如发现创建人 == userId
+		 	 if("w".equals(permission)) {
+				 if(entity.getUserId() == userId) {
+				 	return;
+				 }
+				 
+				 throw new SecurityException("you cannot admin ${className}:"+${classNameLower});
+			 }
+			 */
+			
+			return;
+		}
 	}
 	
 	protected long getLoginUserId(HttpServletRequest request) {
