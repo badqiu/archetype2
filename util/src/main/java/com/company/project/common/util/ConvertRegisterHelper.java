72c0c28c218d8ef0de22ec1f8d4bdb0e6f899aaf
badqiu <badqiu@gmail.com>
2019-12-13 14:14:05 +0800

    1

--- util/src/main/java/com/company/project/common/util/ConvertRegisterHelper.java
+++ util/src/main/java/com/company/project/common/util/ConvertRegisterHelper.java
@@ -5,6 +5,7 @@ import java.math.BigInteger;
 import java.sql.Time;
 import java.sql.Timestamp;
 
+import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.commons.beanutils.converters.BigDecimalConverter;
@@ -26,40 +27,47 @@ import com.github.rapid.common.beanutils.converter.StringConverter;
 
 /**
  * 注册Converter, 用于apache commons BeanUtils.copyProperties()方法中的class类型转换;
+ * 
  * 可以修改此处代码以添加新的Converter
  * @author badqiu
  */
 public class ConvertRegisterHelper implements InitializingBean {
 
 	public static void registerConverters(String... datePatterns) {
-		ConvertUtils.register(new StringConverter(), String.class);
-		//date 
-		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new DateConverter(null),datePatterns),java.util.Date.class);
-		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new SqlDateConverter(null),datePatterns),java.sql.Date.class);
-		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimeConverter(null),datePatterns),Time.class);
-		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimestampConverter(null),datePatterns),Timestamp.class);
-		//number
-		ConvertUtils.register(new BooleanConverter(null), Boolean.class);
-		ConvertUtils.register(new ShortConverter(null), Short.class);
-		ConvertUtils.register(new IntegerConverter(null), Integer.class);
-		ConvertUtils.register(new LongConverter(null), Long.class);
-		ConvertUtils.register(new FloatConverter(null), Float.class);
-		ConvertUtils.register(new DoubleConverter(null), Double.class);
-		ConvertUtils.register(new BigDecimalConverter(null), BigDecimal.class); 
-		ConvertUtils.register(new BigIntegerConverter(null), BigInteger.class);	
+		
+		registerConverters(BeanUtilsBean.getInstance().getConvertUtils(),datePatterns);
+		
+//		ConvertUtils.register(new StringConverter(), String.class);
+//		//date 
+//		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new DateConverter(null),datePatterns),java.util.Date.class);
+//		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new SqlDateConverter(null),datePatterns),java.sql.Date.class);
+//		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimeConverter(null),datePatterns),Time.class);
+//		ConvertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimestampConverter(null),datePatterns),Timestamp.class);
+//		//number
+//		ConvertUtils.register(new BooleanConverter(null), Boolean.class);
+//		ConvertUtils.register(new ShortConverter(null), Short.class);
+//		ConvertUtils.register(new IntegerConverter(null), Integer.class);
+//		ConvertUtils.register(new LongConverter(null), Long.class);
+//		ConvertUtils.register(new FloatConverter(null), Float.class);
+//		ConvertUtils.register(new DoubleConverter(null), Double.class);
+//		ConvertUtils.register(new BigDecimalConverter(null), BigDecimal.class); 
+//		ConvertUtils.register(new BigIntegerConverter(null), BigInteger.class);	
 	}
-
+	
+	static String[] datePattern = new String[] {"yyyy-MM-dd","yyyy-MM-dd HH:mm:ss","yyyy-MM-dd HH:mm:ss.SSS","HH:mm:ss"};
 	public static void registerConverters(ConvertUtilsBean convertUtils) {
-		registerConverters(convertUtils,"yyyy-MM-dd","yyyy-MM-dd HH:mm:ss","yyyy-MM-dd HH:mm:ss.SSS","HH:mm:ss");
+		registerConverters(convertUtils,datePattern);
 	}
 	
 	public static void registerConverters(ConvertUtilsBean convertUtils,String... datePatterns) {
 		convertUtils.register(new StringConverter(), String.class);
+		
 		//date 
 		convertUtils.register(ConvertRegisterHelper.setPatterns(new DateConverter(null),datePatterns),java.util.Date.class);
 		convertUtils.register(ConvertRegisterHelper.setPatterns(new SqlDateConverter(null),datePatterns),java.sql.Date.class);
 		convertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimeConverter(null),datePatterns),Time.class);
 		convertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimestampConverter(null),datePatterns),Timestamp.class);
+		
 		//number
 		convertUtils.register(new BooleanConverter(null), Boolean.class);
 		convertUtils.register(new ShortConverter(null), Short.class);
@@ -78,7 +86,7 @@ public class ConvertRegisterHelper implements InitializingBean {
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		registerConverters("yyyy-MM-dd","yyyy-MM-dd HH:mm:ss","yyyy-MM-dd HH:mm:ss.SSS","HH:mm:ss");
+		registerConverters(datePattern);
 	}
 	
 }
