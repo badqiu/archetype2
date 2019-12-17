92655ee06e8027bbda15a5f1b80785be4637bbfe
badqiu <badqiu@gmail.com>
2019-12-17 13:00:12 +0800

    1

--- mvn-archetype-create-from-project.bat
+++ mvn-archetype-create-from-project.bat
@@ -1,3 +1,12 @@
+echo ----------------------------------------------------------------------------
+echo delete eclipse bin dir
+echo ----------------------------------------------------------------------------
+rmdir /s/q user-server/bin
+rmdir /s/q admin-server/bin
+rmdir /s/q service/bin
+rmdir /s/q dao/bin
+
+
 call mvn package -Dmaven.test.skip=true -U
 call mvn clean
 call mvn eclipse:clean
@@ -6,11 +15,14 @@ call mvn archetype:create-from-project
 echo ----------------------------------------------------------------------------
 echo override some file and delete some file
 echo ----------------------------------------------------------------------------
+
 del target\generated-sources\archetype\src\main\resources\archetype-resources\todo.txt
 del target\generated-sources\archetype\src\main\resources\archetype-resources\release_checklist.txt
 del target\generated-sources\archetype\src\main\resources\archetype-resources\pom-archetype.xml
 del target\generated-sources\archetype\src\main\resources\archetype-resources\mvn-archetype-create-from-project.bat
 del target\generated-sources\archetype\src\main\resources\archetype-resources\archetype-catalog.xml
+rmdir /s/q target\generated-sources\archetype\src\main\resources\archetype-resources\admin-front\node_modules
+rmdir /s/q target\generated-sources\archetype\src\main\resources\archetype-resources\user-front\node_modules
 copy /Y target\generated-sources\archetype\pom.xml target\generated-sources\archetype\pom.bak.xml
 copy /Y pom-archetype.xml target\generated-sources\archetype\pom.xml
 
@@ -18,6 +30,7 @@ copy /Y pom-archetype.xml target\generated-sources\archetype\pom.xml
 echo ----------------------------------------------
 echo install and deploy archetype
 echo ----------------------------------------------
+
 cd target/generated-sources/archetype
 call mvn clean
 call mvn install deploy
@@ -26,7 +39,8 @@ cd ../../../
 echo ----------------------------------------------
 echo "before exec: mvn eclipse:eclipse "
 echo ----------------------------------------------
-pause
+
+REM pause
 call mvn eclipse:eclipse
 call mvn package -Dmaven.test.skip=true
 
--- user-server/src/main/java/com/company/project/controller/BaseController.java
+++ user-server/src/main/java/com/company/project/controller/BaseController.java
@@ -5,12 +5,32 @@ import javax.servlet.http.HttpServletRequest;
 public class BaseController {
 	
 	/**
-	 * 检查实体的读写权限,没有权限抛出 SecurityException
+	 * 检查实体读写权限(数据行读写权限),没有权限抛出 SecurityException
 	 * 
-	 * @param entity 表实体对象，应用一条行记录
+	 * @param entity 实体对象(数据行)
 	 * @param permission 增删修改=w(写权限), 查=r(读权限)
 	 */
-	public void checkEntityPermission(HttpServletRequest request,Object entity,String permission) {
+	protected void checkEntityPermission(HttpServletRequest request,Object entity, String permission) {
+		//检查读权限
+		if("r".equals(permission)){
+			return;
+		}
+		
+		//检查写权限
+		if("w".equals(permission)){
+			/*
+			示例代码,如发现创建人 == userId
+		 	 if("w".equals(permission)) {
+				 if(entity.getUserId() == userId) {
+				 	return;
+				 }
+				 
+				 throw new SecurityException("you cannot admin ${className}:"+${classNameLower});
+			 }
+			 */
+			
+			return;
+		}
 	}
 	
 }
