94e85fec128eee227daa52da4060f67425bbdc15
badqiu <badqiu@gmail.com>
2020-07-31 15:09:48 +0800

    1

--- admin-server/src/main/java/com/company/project/admin/controller/AdminPingController.java
+++ admin-server/src/main/java/com/company/project/admin/controller/AdminPingController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 检查服务是否正常
@@ -11,17 +12,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author dqiu
  *
  */
-@Controller
+@RestController
 @RequestMapping("/admin/ping")
 public class AdminPingController {
 
 	@RequestMapping
-	@ResponseBody
 	public String ping() {
 		//可以添加执行，查询数据
 		
 		System.out.println("ping");
 		return "PONG";
+	}
+	
+	@RequestMapping
+	public String exception() {
+		throw new IllegalStateException("test_error_message");
 	}
 	
 }
