42ba9bd28c325f7e274092a4f1814802f66a8937
badqiu <badqiu@gmail.com>
2020-07-31 16:38:07 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- admin-server/src/main/java/com/company/project/exception/handler/WebExceptionHandler.java
+++ admin-server/src/main/java/com/company/project/exception/handler/WebExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,18 +22,18 @@ public class WebExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(WebExceptionHandler.class);
 
     @ExceptionHandler
-    public ResponseEntity exception(Exception e) {
+    public Map exception(Exception e) {
         log.error("exception", e);
         
         Map<String,String> result = new HashMap<String,String>();
         result.put("errCode", e.getClass().getSimpleName());
         result.put("errMsg", e.getMessage());
         
-        return ResponseEntity.ok(result);
+        return result;
     }
     
     @ExceptionHandler
-    public ResponseEntity securityException(SecurityException e) {
+    public Map securityException(SecurityException e) {
         return exception(e);
     }
     
