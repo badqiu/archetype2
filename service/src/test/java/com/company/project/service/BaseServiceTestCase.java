afb50e7ab1fde51fdc01f1e02b7003d420389735
badqiu <badqiu@gmail.com>
2020-07-31 17:06:50 +0800

    1

---
+++ service/src/test/java/com/company/project/service/BaseServiceTestCase.java
@@ -0,0 +1,37 @@
+package com.company.project.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.company.project.ServiceTestApplication;
+
+
+//@SpringBootTest(classes={
+//		CacheConfig.class,DataSourceConfig.class,
+//		I18nConfig.class,MybatisPlusConfig.class,RedisConfig.class,
+//		TomcatConfig.class,ValidatorConfig.class,WebMvcConfig.class,
+//		DaoConfig.class,ServiceConfig.class,
+//})
+
+
+//@EnableAutoConfiguration
+//@SpringBootApplication
+/*
+@ImportAutoConfiguration({
+	CacheConfig.class,DataSourceConfig.class,
+	I18nConfig.class,MybatisPlusConfig.class,RedisConfig.class,
+	TomcatConfig.class,ValidatorConfig.class,WebMvcConfig.class
+})
+*/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={ServiceTestApplication.class})
+//@ComponentScan(basePackages="com.company.project.dao")
+@Transactional
+public class BaseServiceTestCase {
+
+}
+
+
