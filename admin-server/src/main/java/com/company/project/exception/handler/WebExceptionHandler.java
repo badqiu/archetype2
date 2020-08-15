bdb5ef136db9c5e38c8f26ce7cb3acb018c3d404
badqiu <badqiu@gmail.com>
2020-08-15 12:24:48 +0800

    1

--- admin-server/src/main/java/com/company/project/exception/handler/WebExceptionHandler.java
+++ admin-server/src/main/java/com/company/project/exception/handler/WebExceptionHandler.java
@@ -2,6 +2,11 @@ package com.company.project.exception.handler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,17 +29,45 @@ public class WebExceptionHandler {
     @ExceptionHandler
     public Map exception(Exception e) {
         log.error("exception", e);
-        
-        Map<String,String> result = new HashMap<String,String>();
-        result.put("errCode", e.getClass().getSimpleName());
-        result.put("errMsg", e.getMessage());
+        return errorMap(e, e.getMessage());
+    }
+
+	private Map errorMap(Exception e, String errMsg) {
+		return errorMap(e.getClass().getSimpleName(),errMsg);
+	}
+	
+	private Map errorMap(String errCode, String errMsg) {
+		Map<String,String> result = new HashMap<String,String>();
+		result.put("errCode", errCode);
+		result.put("errMsg", errMsg);
         
         return result;
-    }
+	}
     
     @ExceptionHandler
     public Map securityException(SecurityException e) {
         return exception(e);
+    }
+    
+    @ExceptionHandler
+    public Map constraintViolationException(ConstraintViolationException e) {
+    	Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+    	if(constraintViolations == null) {
+    		return errorMap(e,e.getMessage());
+    	}
+    	
+		String errMsg =  constraintViolations.stream()
+		.map( cv -> cv == null ? "null" : cv.getPropertyPath() + ": " + cv.getMessage() )
+		.collect( Collectors.joining( ", " ) );
+		
+		Map errMap = new HashMap();
+		constraintViolations.forEach( row -> { 
+			errMap.put(row.getPropertyPath(), row.getMessage());
+		});
+		
+		Map map =  errorMap(e,errMsg);
+		map.put("errMap",errMap);
+		return map;
     }
     
 }
--- service/src/main/java/com/company/project/springconfig/ValidatorConfig.java
+++ service/src/main/java/com/company/project/springconfig/ValidatorConfig.java
@@ -1,8 +1,42 @@
 package com.company.project.springconfig;
 
+import javax.validation.Validator;
+
+import org.springframework.boot.validation.MessageInterpolatorFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import com.github.rapid.common.util.holder.BeanValidatorHolder;
 
 @Configuration
 public class ValidatorConfig {
 
+    @Bean
+    public MessageSource validationMessageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setDefaultEncoding("UTF-8");//读取配置文件的编码格式
+        source.setCacheMillis(-1);//缓存时间，-1表示不过期
+        source.setBasename("ValidationMessages");//配置文件前缀名，设置为Messages,那你的配置文件必须以Messages.properties/Message_en.properties...
+        return source;
+    }
+    
+    @Bean
+    public Validator validator() {
+        LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+        MessageInterpolatorFactory interpolatorFactory = new MessageInterpolatorFactory();
+        factoryBean.setMessageInterpolator(interpolatorFactory.getObject());
+        factoryBean.setValidationMessageSource(validationMessageSource());
+        return factoryBean;
+    }
+    
+    @Bean
+    public BeanValidatorHolder beanValidatorHolder() {
+        BeanValidatorHolder beanValidatorHolder = new BeanValidatorHolder();
+        beanValidatorHolder.setValidator(validator());
+		return beanValidatorHolder;
+    }
+    
 }
