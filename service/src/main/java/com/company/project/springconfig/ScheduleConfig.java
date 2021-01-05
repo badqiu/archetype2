c468c76ebf821ca7e2e969775eca4cf809fd8209
badqiu <badqiu@gmail.com>
2021-01-05 15:50:26 +0800

    1

---
+++ service/src/main/java/com/company/project/springconfig/ScheduleConfig.java
@@ -0,0 +1,26 @@
+package com.company.project.springconfig;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(destroyMethod="shutdown")
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(15); //指定线程池大小
+    }
+    
+}
