1ef16ea696929a78c8dd492370a71f4b63e44806
badqiu <badqiu@gmail.com>
2021-01-11 15:38:16 +0800

    1

--- admin-server/src/main/java/com/company/project/web/springconfig/SwaggerConfiguration.java
+++ admin-server/src/main/java/com/company/project/web/springconfig/SwaggerConfiguration.java
@@ -19,24 +19,25 @@
 
 package com.company.project.web.springconfig;
 
-import com.company.project.AdminApplication;
-import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.company.project.AdminApplication;
+import com.google.common.collect.Lists;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
