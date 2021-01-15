eec46bdb37e5f3ad9330205a97e2c0100d966aec
badqiu <badqiu@gmail.com>
2021-01-15 16:30:48 +0800

    1

---
+++ util/src/main/java/com/company/project/common/security/SpringActionSecurityUtil.java
@@ -0,0 +1,60 @@
+package com.company.project.common.security;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class SpringActionSecurityUtil extends ActionSecurityUtil {
+	
+	public static ServletRequestAttributes getContextServletRequestAttributes() {
+		ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		return servletRequestAttributes;
+	}
+	
+	public static HttpServletRequest getRequest() {
+		return getContextServletRequestAttributes().getRequest();
+	}
+	
+	/**
+	 * 检查读写权限,没有权限抛出 SecurityException
+	 * 
+	 * @param actionType 动作分类
+	 * @param permission 增删修改=w(写权限), 查=r(读权限)
+	 */
+	public static void checkActionPermission(Class<?> actionType,String permission) {
+		checkActionPermission(getRequest(),actionType,permission);
+	}
+
+	public static void checkActionPermission(String actionType,String permission) {
+		checkActionPermission(getRequest(),actionType,permission);
+	}
+
+	public static boolean hasActionPermission(Class<?> actionType, String permission) {
+		return hasActionPermission(getRequest(), actionType, permission);
+	}
+	
+	public static boolean hasActionPermission(String actionType, String permission) {
+		return hasActionPermission(getRequest(), actionType, permission);
+	}
+	
+	public static LoginUser getRequiredLoginUser() {
+		return getRequiredLoginUser(getRequest());
+	}
+	
+	public static LoginUser getLoginUser() {
+		return getLoginUser(getRequest());
+	}
+	
+	/** 
+	 *  用户登录成功,设置登录成功信息
+	 **/
+	public static void setLoginUser(LoginUser loginUser) {
+		setLoginUser(getRequest(),loginUser);
+	}
+	
+	public static void logout() {
+		logout(getRequest());
+	}
+	
+}
