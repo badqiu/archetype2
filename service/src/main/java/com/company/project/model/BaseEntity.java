4047fa3bb94cbef4c551be89385d19b252ef3c0f
badqiu <badqiu@gmail.com>
2021-03-03 11:29:24 +0800

    1

---
+++ service/src/main/java/com/company/project/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.company.project.model;
+
+import java.io.Serializable;
+/**
+ * 基本实值，可以添加统一的审计信息，如: creator,operator,updateTime,createTime等字段
+ * 
+ * @author badqiu
+ *
+ */
+public class BaseEntity implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
+
