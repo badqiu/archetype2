f95a8371abdaaa67c183868d2228ccede9e3f20e
qiubaichao <qiubaichao@xmiles.cn>
2023-11-16 17:13:43 +0800

    1

--- admin-server/src/main/java/com/company/project/web/springconfig/SwaggerDocConfig.java
+++ admin-server/src/main/java/com/company/project/web/springconfig/SwaggerDocConfig.java
@@ -32,16 +32,16 @@ import com.google.common.collect.Lists;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
+@EnableOpenApi
+public class SwaggerDocConfig {
 	
     @Bean
     public Docket createRestApi() {
--- service/src/main/java/com/company/project/springconfig/DocConfig.java
+++
@@ -1,8 +0,0 @@
-package com.company.project.springconfig;
-
-import springfox.documentation.oas.annotations.EnableOpenApi;
-
-@EnableOpenApi
-public class DocConfig {
-
-}
