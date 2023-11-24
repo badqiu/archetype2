3f53c2c6f20e61cc47754839cb317830b558ba09
qiubaichao <qiubaichao@xmiles.cn>
2023-11-24 13:34:41 +0800

    增加Aop

--- service/src/main/java/com/company/project/query/BaseQuery.java
+++ service/src/main/java/com/company/project/query/BaseQuery.java
@@ -4,6 +4,8 @@ import com.github.rapid.common.util.page.PageQuery;
 
 public class BaseQuery extends PageQuery{
 
+	private static final long serialVersionUID = 1L;
+
 	public BaseQuery() {
 		super();
 	}
