41769dc86ae1d630f2a78397a4cf2e04aecf403c
qiubaichao <qiubaichao@xmiles.cn>
2023-12-05 10:02:34 +0800

    1

---
+++ service/src/main/java/com/company/project/springconfig/AopCreatorOperatorConfig.java
@@ -0,0 +1,94 @@
+package com.company.project.springconfig;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.company.project.common.security.ActionSecurityUtil;
+import com.github.rapid.common.beanutils.BeanUtils;
+
+/**
+ * 为对象统一注入 creator,operator,createTime,updateTime,enabled
+ */
+@Aspect
+@Component
+public class AopCreatorOperatorConfig {
+	private static Logger logger = LoggerFactory.getLogger(AopCreatorOperatorConfig.class);
+	
+    @Pointcut("execution(* com.bigdata.ai.service.*.updateByManual(..))")
+    public void serviceUpdate(){
+    }
+    
+    @Pointcut("execution(* com.bigdata.ai.service.*.create(..))")
+    public void serviceCreate(){
+    }
+    
+    @Pointcut("serviceUpdate() || serviceCreate()")
+    private void serviceUpdateCreate(){}
+    
+    
+    @Around("serviceUpdateCreate()")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+    	
+        Signature signature = pjp.getSignature();
+		String methodName = signature.getName();
+
+		Object[] args = pjp.getArgs();
+        Object target = args[0];
+        
+        Map<String,Object> sourceProps = newBeanProps(methodName);
+        
+//        BeanUtils.copyProperties(beanProps, firstArg);
+        BeanUtils.copyProperties(target,sourceProps);
+        logger.info("around for method:"+methodName+"."+signature.getDeclaringTypeName()+" beanProps:"+sourceProps+" firstArgs:"+target);
+        
+        Object result = pjp.proceed();
+        
+        return result;
+    }
+
+	private Map<String,Object> newBeanProps(String methodName) {
+		Map<String,Object> beanProps = new HashMap<String,Object>();
+		String username = ActionSecurityUtil.getLoginUser(getRequest()).getUsername();
+        
+		if("create".equals(methodName)){
+			beanProps.put("creator", username);
+	        beanProps.put("operator",username);
+	        beanProps.put("createTime",new Date());
+	        beanProps.put("updateTime",new Date());
+	        beanProps.put("enabled",true);
+        }else {
+        	beanProps.put("operator",username);
+        	beanProps.put("updateTime",new Date());
+        }
+        
+		return beanProps;
+	}
+	
+	public static HttpServletRequest getRequest() {
+		return getContextServletRequestAttributes().getRequest();
+	}
+	
+	public static HttpServletResponse getResponse() {
+		return getContextServletRequestAttributes().getResponse();
+	}
+
+	public static ServletRequestAttributes getContextServletRequestAttributes() {
+		ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		return servletRequestAttributes;
+	}
+}
