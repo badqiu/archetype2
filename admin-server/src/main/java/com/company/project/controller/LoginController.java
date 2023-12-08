72e74d916fcf1c854ca09c2968c8993919f7250e
qiubaichao <qiubaichao@xmiles.cn>
2023-12-08 13:35:32 +0800

    loginUser

--- admin-server/src/main/java/com/company/project/controller/LoginController.java
+++ admin-server/src/main/java/com/company/project/controller/LoginController.java
@@ -2,9 +2,13 @@ package com.company.project.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.company.project.common.security.LoginUser;
+import com.company.project.common.security.SpringActionSecurityUtil;
 
 import io.swagger.annotations.Api;
 
@@ -19,6 +23,11 @@ public class LoginController {
 		
 		System.out.println("login success:"+username);
 		return true;
+	}
+	
+	@GetMapping
+	public LoginUser loginUser() {
+		return SpringActionSecurityUtil.getLoginUser();
 	}
 	
 	@RequestMapping
