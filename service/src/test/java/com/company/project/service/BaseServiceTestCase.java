c21ac5705fab701776193c2073bd54935c7f3e6f
qiubaichao <qiubaichao@xmiles.cn>
2023-12-20 17:43:19 +0800

    1

--- service/src/test/java/com/company/project/service/BaseServiceTestCase.java
+++ service/src/test/java/com/company/project/service/BaseServiceTestCase.java
@@ -2,7 +2,6 @@ package com.company.project.service;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
