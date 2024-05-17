2285f6c6c239253339422e87c534b85e119e00d0
qiubaichao <qiubaichao@xmiles.cn>
2024-05-17 17:00:58 +0800

    ConstantTest

---
+++ service/src/test/java/com/company/project/enums/ConstantTest.java
@@ -0,0 +1,13 @@
+package com.company.project.enums;
+
+import org.junit.Test;
+
+public class ConstantTest {
+
+	@Test
+	public void printAppName() {
+		System.out.println("APP_NAME:" + Constant.APP_NAME);
+	}
+	
+
+}
