9dcc2e2237d91bc75f5f5b38c8a7cd14f92c2ee5
qiubaichao <qiubaichao@xmiles.cn>
2024-05-30 10:37:29 +0800

    @JdbcTest

--- service/src/test/java/com/company/project/dao/impl/BaseDaoTestCase.java
+++ service/src/test/java/com/company/project/dao/impl/BaseDaoTestCase.java
@@ -2,9 +2,9 @@ package com.company.project.dao.impl;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 
 import com.company.project.ServiceTestApplication;
 
@@ -12,6 +12,7 @@ import com.company.project.ServiceTestApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={ServiceTestApplication.class})
+@JdbcTest
 @Transactional
 public abstract class BaseDaoTestCase {
 
