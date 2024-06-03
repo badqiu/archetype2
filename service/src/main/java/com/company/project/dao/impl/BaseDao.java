e3c6e32a3975daf0d1a45e84475f285c0f34d088
qiubaichao <qiubaichao@xmiles.cn>
2024-06-03 14:48:20 +0800

    BaseSpringJdbcDao

--- service/src/main/java/com/company/project/dao/impl/BaseDao.java
+++ service/src/main/java/com/company/project/dao/impl/BaseDao.java
@@ -2,10 +2,11 @@ package com.company.project.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.company.project.dao.Dao;
 import com.github.rapid.common.jdbc.dao.support.BaseSpringJdbcDao;
 import com.github.rapid.common.jdbc.dialect.Dialect;
 
-public abstract class BaseDao extends BaseSpringJdbcDao{
+public abstract class BaseDao extends BaseSpringJdbcDao implements Dao{
 
 	@Autowired
 	public void setDialect(Dialect d) {
