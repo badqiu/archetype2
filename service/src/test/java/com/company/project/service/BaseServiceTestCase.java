93f84dca3e91846b3b48e63a8482dd688255c2fa
qiubaichao <qiubaichao@xmiles.cn>
2024-06-03 14:49:41 +0800

    BaseServiceTestCase

--- service/src/test/java/com/company/project/service/BaseServiceTestCase.java
+++ service/src/test/java/com/company/project/service/BaseServiceTestCase.java
@@ -8,26 +8,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.company.project.ServiceTestApplication;
 
 
-//@SpringBootTest(classes={
-//		CacheConfig.class,DataSourceConfig.class,
-//		I18nConfig.class,MybatisPlusConfig.class,RedisConfig.class,
-//		TomcatConfig.class,ValidatorConfig.class,WebMvcConfig.class,
-//		DaoConfig.class,ServiceConfig.class,
-//})
 
 
-//@EnableAutoConfiguration
-//@SpringBootApplication
-/*
-@ImportAutoConfiguration({
-	CacheConfig.class,DataSourceConfig.class,
-	I18nConfig.class,MybatisPlusConfig.class,RedisConfig.class,
-	TomcatConfig.class,ValidatorConfig.class,WebMvcConfig.class
-})
-*/
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={ServiceTestApplication.class})
-//@ComponentScan(basePackages="com.company.project.service")
 @Transactional
 public abstract class BaseServiceTestCase {
 
