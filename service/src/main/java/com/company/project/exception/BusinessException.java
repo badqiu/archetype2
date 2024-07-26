bbcdaa974c46fb38e73005586742deac8ae426e2
qiubaichao <qiubaichao@xmiles.cn>
2024-07-26 09:25:43 +0800

    BusinessException

---
+++ service/src/main/java/com/company/project/exception/BusinessException.java
@@ -0,0 +1,30 @@
+package com.company.project.exception;
+
+/**
+ * 业务异常: BusinessException
+ */
+public class BusinessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public BusinessException() {
+		super();
+	}
+
+	public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public BusinessException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BusinessException(String message) {
+		super(message);
+	}
+
+	public BusinessException(Throwable cause) {
+		super(cause);
+	}
+
+}
