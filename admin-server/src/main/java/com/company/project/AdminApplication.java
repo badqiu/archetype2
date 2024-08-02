01f9d90f3ee8c9cf5fb5cb1f54a6ad7671a3732f
qiubaichao <qiubaichao@xmiles.cn>
2024-08-02 14:22:01 +0800

    AdminApplication

--- admin-server/src/main/java/com/company/project/AdminApplication.java
+++ admin-server/src/main/java/com/company/project/AdminApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.StandardEnvironment;
+
+import com.company.project.util.EnvironmentUtil;
 
 @SpringBootApplication(
 // scanBasePackageClasses =
@@ -22,14 +25,17 @@ public class AdminApplication {
 	 * 测试 RUN 1、http://localhost:8080/user/test1
 	 */
 	public static void main(String[] args) {
+		StandardEnvironment environment = new StandardEnvironment();
+		new EnvironmentUtil().setEnvironment(environment);
+		
+		
 		SpringApplication app = new SpringApplication(AdminApplication.class);
 		app.setBannerMode(Banner.Mode.OFF);
-		
+		app.setEnvironment(environment);
 		// app.setAdditionalProfiles("demo-submodule"); //附加profile激活，用于子模块添加不同配置
 		
 		ConfigurableApplicationContext context = app.run(args);
-		System.out.println("start spring boot success: applicationName:" + context.getApplicationName() + " startupDate:"
-				+ new Timestamp(context.getStartupDate()));
+		System.out.println("start spring boot success: applicationName:" + context.getApplicationName() + " startupDate:"+ new Timestamp(context.getStartupDate()));
 	}
 	
 }
