0869919764e17d0717428f0bb072311bc7730557
qiubaichao <qiubaichao@xmiles.cn>
2024-08-28 18:26:49 +0800

    add cache

--- service/src/main/java/com/company/project/springconfig/CacheConfig.java
+++ service/src/main/java/com/company/project/springconfig/CacheConfig.java
@@ -1,8 +1,29 @@
 package com.company.project.springconfig;
 
+import java.time.Duration;
+
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
 
 @Configuration
 public class CacheConfig {
 
+	@Bean
+	public CaffeineCacheManager caffeineCacheManager() {
+		CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+		
+		Caffeine caffeine = Caffeine.newBuilder()
+                //maximumSize用来控制cache的最大缓存数量
+                .maximumSize(10000)
+                .expireAfterWrite(Duration.ofMinutes(1))
+                ;
+		cacheManager.setCaffeine(caffeine);
+		
+//		cacheManager.setCacheNames(cacheNames);
+		return cacheManager;
+	}
+	
 }
