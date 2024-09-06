d0ddeaa797f8459852791d189a3fe9e955541098
qiubaichao <qiubaichao@xmiles.cn>
2024-09-06 11:35:38 +0800

    isTooManySameNotify

--- admin-server/src/main/java/com/company/project/cron/BaseJob.java
+++ admin-server/src/main/java/com/company/project/cron/BaseJob.java
@@ -1,7 +1,11 @@
 package com.company.project.cron;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.github.rapid.common.util.NotifyUtil;
 
 public abstract class BaseJob {
 	private static Logger logger = LoggerFactory.getLogger(BaseJob.class);
@@ -10,6 +14,13 @@ public abstract class BaseJob {
 	
 	public static void sendNotifyMsg(String title,String content) {
 		logger.info("sendNotifyMsg() title:{} content:{}",title,content);
+		
+		//避免太多相同通知
+		String notifyIntervalKey = "sendNotifyMsg():" + title;
+		if(NotifyUtil.isTooManySameNotify(notifyIntervalKey , Duration.ofMinutes(60))) {
+			return;
+		}
+		
 	}
 	
 }
