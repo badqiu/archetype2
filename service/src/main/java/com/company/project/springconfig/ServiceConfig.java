01457f0de03e6c566ef95565f46cad2e1aec0b67
qiubaichao <qiubaichao@xmiles.cn>
2024-09-20 18:00:03 +0800

    convertRegisterHelper

--- service/src/main/java/com/company/project/springconfig/ServiceConfig.java
+++ service/src/main/java/com/company/project/springconfig/ServiceConfig.java
@@ -1,8 +1,16 @@
 package com.company.project.springconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.company.project.util.ConvertRegisterHelper;
 
 @Configuration
 public class ServiceConfig {
 
+	@Bean
+	public ConvertRegisterHelper convertRegisterHelper() {
+		return new ConvertRegisterHelper();
+	}
+	
 }
