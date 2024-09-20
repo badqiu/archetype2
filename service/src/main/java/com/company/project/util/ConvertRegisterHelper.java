b97bbf0222d96271a220445e7709463c5abea265
qiubaichao <qiubaichao@xmiles.cn>
2024-09-20 17:59:00 +0800

    ConvertRegisterHelper

--- service/src/main/java/com/company/project/util/ConvertRegisterHelper.java
+++ service/src/main/java/com/company/project/util/ConvertRegisterHelper.java
@@ -6,7 +6,6 @@ import java.sql.Time;
 import java.sql.Timestamp;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
-import org.apache.commons.beanutils.ConvertUtils;
 import org.apache.commons.beanutils.ConvertUtilsBean;
 import org.apache.commons.beanutils.converters.BigDecimalConverter;
 import org.apache.commons.beanutils.converters.BigIntegerConverter;
@@ -23,6 +22,7 @@ import org.apache.commons.beanutils.converters.SqlTimeConverter;
 import org.apache.commons.beanutils.converters.SqlTimestampConverter;
 import org.springframework.beans.factory.InitializingBean;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.github.rapid.common.beanutils.converter.StringConverter;
 
 /**
@@ -33,10 +33,20 @@ import com.github.rapid.common.beanutils.converter.StringConverter;
  */
 public class ConvertRegisterHelper implements InitializingBean {
 
-	static String[] datePattern = new String[] {"yyyy-MM-dd","yyyy-MM-dd HH:mm:ss","yyyy-MM-dd HH:mm:ss.SSS","HH:mm:ss"};
+	public static String[] datePattern = new String[] {"yyyy-MM-dd",
+			"yyyy-MM-dd HH:mm:ss",
+			"yyyy-MM-dd HH:mm:ss.SSS","HH:mm:ss",
+			"EEE MMM dd HH:mm:ss 'CST' yyyy"};
 
+	static EnumConvertUtilsBean convertUtilsBean = new EnumConvertUtilsBean();
+	static BeanUtilsBean instance = new BeanUtilsBean(convertUtilsBean);
+	static {
+		BeanUtilsBean.setInstance(instance);
+		registerConverters(datePattern);
+	}
+	
 	public static void registerConverters(String... datePatterns) {
-		registerConverters(BeanUtilsBean.getInstance().getConvertUtils(),datePatterns);
+		registerConverters(convertUtilsBean,datePatterns);
 	}
 	
 	public static void registerConverters(ConvertUtilsBean convertUtils) {
@@ -47,10 +57,10 @@ public class ConvertRegisterHelper implements InitializingBean {
 		convertUtils.register(new StringConverter(), String.class);
 		
 		//date 
-		convertUtils.register(setDatePatterns(new DateConverter(null),datePatterns),java.util.Date.class);
-		convertUtils.register(setDatePatterns(new SqlDateConverter(null),datePatterns),java.sql.Date.class);
-		convertUtils.register(setDatePatterns(new SqlTimeConverter(null),datePatterns),Time.class);
-		convertUtils.register(setDatePatterns(new SqlTimestampConverter(null),datePatterns),Timestamp.class);
+		convertUtils.register(ConvertRegisterHelper.setPatterns(new DateConverter(null),datePatterns),java.util.Date.class);
+		convertUtils.register(ConvertRegisterHelper.setPatterns(new SqlDateConverter(null),datePatterns),java.sql.Date.class);
+		convertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimeConverter(null),datePatterns),Time.class);
+		convertUtils.register(ConvertRegisterHelper.setPatterns(new SqlTimestampConverter(null),datePatterns),Timestamp.class);
 		
 		//number
 		convertUtils.register(new BooleanConverter(null), Boolean.class);
@@ -63,14 +73,28 @@ public class ConvertRegisterHelper implements InitializingBean {
 		convertUtils.register(new BigIntegerConverter(null), BigInteger.class);	
 	}
 	
-	public static <T extends DateTimeConverter> T setDatePatterns(T converter ,String... patterns) {
+	public static <T extends DateTimeConverter> T setPatterns(T converter ,String... patterns) {
 		converter.setPatterns(patterns);
 		return converter;
 	}
 
 	@Override
-	public void afterPropertiesSet() throws Exception {
+	public void afterPropertiesSet()  {
 		registerConverters(datePattern);
+	}
+	
+	public static class EnumConvertUtilsBean extends ConvertUtilsBean {
+	    @Override
+	    public Object convert(String value, Class clazz) {
+	        if (clazz.isEnum()) {
+	        	if(StringUtils.isBlank(value)) {
+		    		return null;
+		    	}
+	            return Enum.valueOf(clazz, value);
+	        } else {
+	            return super.convert(value, clazz);
+	        }
+	    }
 	}
 	
 }
