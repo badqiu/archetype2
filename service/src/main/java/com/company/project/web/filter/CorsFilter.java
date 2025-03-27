c17b6f0c2bde0d79815a3dbb4d2589525589a71f
qiubaichao <qiubaichao@lizhi.fm>
2025-03-28 00:15:14 +0800

    add corsfilter

---
+++ service/src/main/java/com/company/project/web/filter/CorsFilter.java
@@ -0,0 +1,38 @@
+package com.company.project.web.filter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE) // 设置最高优先级
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, 
+                                  HttpServletResponse response, 
+                                  FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        // 设置CORS响应头
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", 
+                "authorization, content-type, xsrf-token, x-requested-with");
+        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        
+        // 处理预检请求(OPTIONS)
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
