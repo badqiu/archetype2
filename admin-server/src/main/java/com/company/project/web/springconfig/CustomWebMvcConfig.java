8ecb67ef27eec4442ec68e7dfce8d80315ab7ed5
qiubaichao <qiubaichao@lizhi.fm>
2025-06-05 20:39:07 +0800

    use CustomWebMvcConfig

--- admin-server/src/main/java/com/company/project/web/springconfig/CustomWebMvcConfig.java
+++ admin-server/src/main/java/com/company/project/web/springconfig/CustomWebMvcConfig.java
@@ -1,18 +1,16 @@
 package com.company.project.web.springconfig;
-
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.fasterxml.jackson.core.JsonParser.Feature;
@@ -24,43 +22,35 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.github.rapid.common.json.jackson.BigintToStringSerializer;
 import com.github.rapid.common.web.mvc.annotation.AutoMethod2PathAnnotationHandlerMapping;
 
-
 /**
  * 主要功能:  约定大于配置的URL映射。 
  * 自动映射: 
  * @RequestMapping  stop()  => /stop
  * 
  * 
- * SpringBoot升级到2.0以后配置文件继承的WebMvcConfigurerAdapter已废除，需要改为WebMvcConfigurationSupport
- * addInterceptors()方法覆写不变，但getRequestMappingHandlerMapping() 需要改为createRequestMappingHandlerMapping(), 自定义的RequestMappingHandlerMapping才能生效
- *    参考：https://deeplyloving.iteye.com/blog/2173927
- *    
  * @author badqiu
- * @date 2019年7月11日
  *
  */
-//@Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
-
-	private static Logger logger = LoggerFactory.getLogger(WebMvcConfig.class);
-
+@Configuration
+public class CustomWebMvcConfig implements WebMvcRegistrations, WebMvcConfigurer{
+    
+    @Override
+    public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+        AutoMethod2PathAnnotationHandlerMapping r = new AutoMethod2PathAnnotationHandlerMapping();
+		return r;
+    }
+    
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		// 解决返回结果乱码问题
-		super.configureMessageConverters(converters);
-//		converters.add(responseBodyConverter());
-		converters.add(responseJsonBodyConverter());
+//		converters.clear();
+//		converters.addFirst(responseJsonBodyConverter());
+//		converters.addFirst(new ByteArrayHttpMessageConverter());
+		
+        converters.removeIf(converter -> converter instanceof StringHttpMessageConverter);
+	        
 	}
-
-	public HttpMessageConverter<String> responseBodyConverter() {
-		StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
-		List<MediaType> mediaTypes = new ArrayList<MediaType>();
-		mediaTypes.add(MediaType.TEXT_HTML);
-		mediaTypes.add(MediaType.TEXT_PLAIN);
-		converter.setSupportedMediaTypes(mediaTypes);
-		return converter;
-	}
-
+	
+	
 	public MappingJackson2HttpMessageConverter responseJsonBodyConverter() {
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(objectMapper());
 		List<MediaType> mediaTypes = new ArrayList<MediaType>();
@@ -100,37 +90,17 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 //	    objectMapper.setDateFormat(new SimpleDateFormat(Constants.DEFAULT_DATE_TIME_FORMAT));
 	    return objectMapper;
 	}
-
 	
-	@Override
-	protected RequestMappingHandlerMapping createRequestMappingHandlerMapping() {
-		// controller映射加强, 自动映射: @RequestMapping  stop()  => /stop
-		return new AutoMethod2PathAnnotationHandlerMapping();
-	}
-
-	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// 访问静态资源
-        registry.addResourceHandler("/**")
-				//project
-                .addResourceLocations("classpath:/resources/")
-                .addResourceLocations("classpath:/public/")
-                .addResourceLocations("classpath:/static/")
-                .addResourceLocations("classpath:/META-INF/resources/")
-                ;
-        
-        
-     // 修复：Springdoc 的正确资源路径（需替换版本号）
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/5.17.14/"); // [!code focus]
-        
-        // 修复：添加 API 描述文件访问路径
-        registry.addResourceHandler("/v3/api-docs/**") // [!code focus]
-                .addResourceLocations("classpath:/META-INF/resources/v3/api-docs/"); //       
-        
-        super.addResourceHandlers(registry);
-    }
-//	
-
-	
+//	@Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		// 访问静态资源
+//        registry.addResourceHandler("/**")
+//				//project
+//                .addResourceLocations("classpath:/resources/")
+//                .addResourceLocations("classpath:/public/")
+//                .addResourceLocations("classpath:/static/")
+//                .addResourceLocations("classpath:/META-INF/resources/")
+//                ;
+//    }
+    
 }
