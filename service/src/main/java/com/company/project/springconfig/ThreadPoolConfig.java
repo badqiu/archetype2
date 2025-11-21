b639d8358a48dde7c466bb8ae1307e5a40746e9d
qiubaichao <qiubaichao@lizhi.fm>
2025-11-21 10:47:40 +0800

    ThreadPoolConfig

---
+++ service/src/main/java/com/company/project/springconfig/ThreadPoolConfig.java
@@ -0,0 +1,33 @@
+package com.company.project.springconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @description: 线程池配置
+ **/
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    @Primary
+    public ThreadPoolExecutor threadPoolExecutor(){
+
+        return new ThreadPoolExecutor( 200,
+                1000,
+                10,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100000),
+                Executors.defaultThreadFactory(),
+                //直接自己调用
+                new ThreadPoolExecutor.CallerRunsPolicy());
+
+    }
+    
+}
